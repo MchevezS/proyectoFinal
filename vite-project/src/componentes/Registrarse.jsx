078ca9a0aaf773c54b4ceb864f30242249ee4603
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-06-26 21:17:00 -0600

    acá hice el registrase, inputs, boton, titulo.

---
+++ vite-project/src/componentes/Registrarse.jsx
@@ -0,0 +1,15 @@
+const Registrarse = ()=>{
+    return(
+        <>
+
+         <h1> Registrate! </h1>
+         <input placeholder="Nombre completo"/>
+         <input placeholder="Correo electronico"/>
+         <input placeholder="Ingrese su clave"/>
+         <input placeholder="Confirme su clave"/>
+
+         <button>Registrarse.</button>
+        </>
+    )
+}
+export default Registrarse
