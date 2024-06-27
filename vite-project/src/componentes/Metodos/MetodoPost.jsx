13e3a896a94169834d4029bb1abc9910dcffefd7
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-06-27 09:55:42 -0600

    METODO POST: su estructura esta completa

--- vite-project/src/componentes/Metodos/MetodoPost.jsx
+++ vite-project/src/componentes/Metodos/MetodoPost.jsx
@@ -1,3 +1,23 @@
-const MetodoPost = ()=>{
+
+
+
+async function MetodoPost() {
+    try {
+        const guardarDatos = {
+         id:Date.now(),
+         nombre: input.value,
+         estado: false
+        }
+        const response = await fetch(" http://localhost:3001",{
+            method: "POST",
+            headers:{
+                 "Content-type": "application/json; charset=UTF-8"
+            },
+            body:JSON.stringify(guardarDatos)
+        })
+        console.log(`La tarea ${tarea.id} fue agregada`);
     
+    } catch (error) {
+        console.error(error);
+    }
 }
