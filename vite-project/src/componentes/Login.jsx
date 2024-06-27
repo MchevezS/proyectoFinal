e31ab0391b2e37a99555896c876ea5ce7ec0467f
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-06-26 21:02:13 -0600

    login: inputs y button

--- vite-project/src/componentes/Login.jsx
+++ vite-project/src/componentes/Login.jsx
@@ -1,10 +1,16 @@
 const Login = ()=>{
     return(
         <>
+
             <h1>Bienvenidos</h1>
-             <h2>No tienes cuenta?</h2>
+            <input placeholder= "Nmbre de usuario"/>
+            <input placeholder= "Password"/>
+            <button onClick={Login}>Iniciar sesión</button>
+
+             <a href="Registrarse">No tienes cuenta?</a>
 
             
         </>
     )
 }
+export default Login
