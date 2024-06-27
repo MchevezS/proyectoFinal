59d5402f9429c54bce40bb165c922f20821ba823
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-06-27 08:21:42 -0600

    Hice lo de las rutas

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Login from './componentes/Login.jsx'
+import Registrarse from './componentes/Registrarse.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Login/>
+  },
+  {
+    path:"/Registro",
+    element:<Registrarse/>
+  },
+  {
+    path:""
+  }
+
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>,
 )
