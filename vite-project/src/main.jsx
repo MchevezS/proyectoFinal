298fa1f729a70bb2c63862f985777dacf67eaf97
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-06-28 10:59:32 -0600

    main volvio a su lugar

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../App.jsx'
+import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Login from '../componentes/Login.jsx'
-import Registrarse from '../componentes/Registrarse.jsx'
+import Login from './componentes/Login.jsx'
+import Registrarse from './componentes/Registrarse.jsx'
 
 
 const router = createBrowserRouter([
