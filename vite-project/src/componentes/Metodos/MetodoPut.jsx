fe5405dcc77683cdd948bd91bcd2bc5c1de9bd71
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-07-01 08:20:35 -0600

    Metodo Put

--- vite-project/src/componentes/Metodos/MetodoPut.jsx
+++ vite-project/src/componentes/Metodos/MetodoPut.jsx
@@ -1,8 +1,17 @@
 async function MetodoPut() {
     try {
-        const response = await fetch ("http://localhost:3001")
-        
+        const response = await fetch ("http://localhost:3001",{
+            method: "POST",
+            headers:{
+                 "Content-type": "application/json; charset=UTF-8"
+            },
+            body:JSON.stringify()
+        })
+        const datos = await response.json()
+        console.log(datos);
+        return datos        
     } catch (error) {
+        console.error(error);
         
     }
 }
