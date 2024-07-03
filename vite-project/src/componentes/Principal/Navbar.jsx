184b282ad8562b896849a591d866b950a5233391
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-07-03 09:01:00 -0600

    navbar, todo loo qe va a contener mi navbar

--- vite-project/src/componentes/Principal/Navbar.jsx
+++ vite-project/src/componentes/Principal/Navbar.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 
 export default function Navbar() {
 
-  return (
+  return (<>
+   <navbar>
+        {/* icono */}
+      <p>Soporte al cliente </p>
+        <p>255-686-4769</p>
+       <p>CAFETERÍA FWD-COSTA RICA</p>
+      </navbar>
     <navbar>
     <ul>
         <li>Inicio</li>
-        <li>Moca  chino</li>
-        <li>Café frio</li>
-        <li></li>
+        <li>Moca Helado</li>
+        <li>Capuchino</li>
+        <li>Expreso</li>
+        <li>Postres</li>
+        <li>Más</li>
+        <input type='text' placeholder='Buscar'/>
     </ul>
     </navbar>
+    </>
   )
 }
