ff4566094bb3c1597ebe8b936b57ad818432455c
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-07-03 14:12:29 -0600

    cambie el nombre del index. En info Cafteria agregue informacion acerca de nosotros

--- vite-project/src/componentes/Principal/InfoCafeteria.jsx
+++ vite-project/src/componentes/Principal/InfoCafeteria.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function InfoCafeteria() {
   return (
     <div>
-    
+    <div>
+        <p>Acerca de Nosotros</p>   
+        <p>Informacion Delivery</p>
+    </div>
       
     </div>
   )
