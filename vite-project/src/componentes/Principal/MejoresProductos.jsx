46465b1f4acaebcc0e396cbccfd53df309f00985
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-07-04 10:18:38 -0600

    oordenando los mejores productos

--- vite-project/src/componentes/Principal/MejoresProductos.jsx
+++ vite-project/src/componentes/Principal/MejoresProductos.jsx
@@ -4,9 +4,15 @@ export default function MejoresProductos() {
   return (<>
     <div>  {/*aca estoy separando los productos por seccion */}
     <h1>Mejores productos</h1>
+    <th>
       <h4>Destacados</h4>
+      </th>
+      <th>
       <h4> Más recientes</h4>
+      </th>
+      <th>
       <h4>Mejores vendidos</h4>
+      </th>
     </div>
     <h1>Especiales</h1>
     {/* voy agregar las imganes de los productos especiales */}
