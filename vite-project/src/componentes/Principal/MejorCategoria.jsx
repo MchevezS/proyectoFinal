d0163e903245b17a662d6a07d51da58f6601a0ba
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-07-14 21:17:47 -0600

    estilos en mejor categoria

--- vite-project/src/componentes/Principal/MejorCategoria.jsx
+++ vite-project/src/componentes/Principal/MejorCategoria.jsx
@@ -14,23 +14,28 @@ export default function MejorCategoria() {
         <p className='cafeMoca'>Café Moca</p>
         <h4 className='cafeLinea1'>_______</h4>
         <h4 className='verMas1'>Ver mas</h4>
+        <img className='mocaImagen' src='./src/Imagenes/OIP.jpg'></img>
     </th>
     <th> 
-         <p className='espreso'>Expreso Americano</p>
+         <p className='expreso1'>Expreso Americano</p>
          <h4 className='cafeLinea2'>_______</h4>
          <h4 className='verMas2'>Ver mas</h4>
+        <img className='imagenExpreso' src='./src/Imagenes/expreso1.jpg'></img>
     </th>
     <th> 
            <p  className='capuchino1'>Capuchino</p>
            <h4  className='cafeLinea3'>_______</h4>
            <h4  className='verMas3'>Ver mas</h4>
+           <img className='imagenCapuchino' src='./src/Imagenes/capuchino.jpg'></img>
      </th>
      <th> 
            <p  className='postre'>Postres</p>
            <h4  className='cafeLinea4'>_______</h4>
            <h4  className='verMas4'>Ver mas</h4>
+           <img className='imagenPostre' src='./src/Imagenes/tresLeche.jpg'></img>
     </th>
      
+
     </>
   )
 }
